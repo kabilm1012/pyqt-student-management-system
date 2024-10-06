de60c3c1ba8099eaa3cb8281174380d0c605abc9
Kabil M <kabilm1012@gmail.com>
2024-10-06 12:54:07 +0530

    Populate table with data

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from PyQt6.QtCore import Qt
 from PyQt6.QtWidgets import QLabel, QWidget, QApplication, QGridLayout, \
-    QPushButton, QLineEdit, QMainWindow, QTableWidget
+    QPushButton, QLineEdit, QMainWindow, QTableWidget, QTableWidgetItem
 from PyQt6.QtGui import QAction
 import sys
+import sqlite3
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -22,10 +23,19 @@ class MainWindow(QMainWindow):
         self.table = QTableWidget()
         self.table.setColumnCount(4)
         self.table.setHorizontalHeaderLabels(("ID", "Name", "Course", "Mobile"))
+        self.table.verticalHeader().setVisible(False)
         self.setCentralWidget(self.table)
+        self.load_data()
 
-    def load_data():
-        pass
+    def load_data(self):
+        connection = sqlite3.connect("database.db")
+        result = connection.execute("SELECT * FROM students")
+        self.table.setRowCount(0)
+        for row_number, row_data in enumerate(list(result)):
+            self.table.insertRow(row_number)
+            for column_number, data in enumerate(row_data):
+                self.table.setItem(row_number, column_number, QTableWidgetItem(str(data)))
+        connection.close()
 
 
 app = QApplication(sys.argv)
